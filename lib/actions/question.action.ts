005d395952596e0244bda13ac85ef248ed86f93c
Afifah Salimi <afifahsalimi@gmail.com>
2023-12-18 22:15:11 +0800

    integrate create question api

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,49 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose";
+import { CreateQuestionParams } from "./shared.types";
 
-export async function createQuestion() {
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     await connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    const question = await Question.create({ title, content, author });
+
+    const tagDocuments = [];
+
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        {
+          name: { $regex: new RegExp(`^${tag}$`, "i") },
+        },
+        {
+          $setOnInsert: { name: tag },
+          $push: { question: question._id },
+        },
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // Create an interaction record for the user's ask_question action
+
+    // Increment author's reputation by +5 for creating a question
+
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
   }
