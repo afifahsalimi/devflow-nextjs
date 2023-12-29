2b97ca1978900e04e64eaf84f8144f87a1b4b3ec
Afifah Salimi <afifahsalimi@gmail.com>
2023-12-29 11:52:45 +0800

    updated middleware routes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
   webpack: (config) => {
     Object.assign(config.resolve.alias, {
       aws4: false,
